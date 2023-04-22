6ade356a11283382fbeb59af38ea09efa74ab219
fairlight1337 <jan.winkler.84@gmail.com>
2023-04-22 11:37:10 +0200

    Adjusted the solve() algorithm to take food into account
    The solve() method should now correctly be able to find a path through
    the maze, given that food plays a role.

--- src/maze.cpp
+++ src/maze.cpp
@@ -110,9 +110,11 @@ std::vector<Maze::Move> Maze::solve() {
   std::unordered_set<Coordinates, std::hash<Coordinates>> closedSet;
   std::unordered_map<Coordinates, Coordinates, std::hash<Coordinates>> cameFrom;
   std::unordered_map<Coordinates, int, std::hash<Coordinates>> gScore;
+  std::unordered_map<Coordinates, int, std::hash<Coordinates>> foodMap;
 
-  openSet.push({manhattanDistance(startPos, endPos), startPos});
+  openSet.push({manhattanDistance(startPos, endPos) + localPlayer.getCurrentFood(), startPos});
   gScore[startPos] = 0;
+  foodMap[startPos] = localPlayer.getCurrentFood();
 
   while (!openSet.empty()) {
     Coordinates current = openSet.top().second;
@@ -139,11 +141,24 @@ std::vector<Maze::Move> Maze::solve() {
     for (const auto &neighbor : getNeighbors(current)) {
       if (closedSet.count(neighbor) == 0) {
         int tentativeGScore = gScore[current] + 1;
+        int food = foodMap[current] - 1;
+
+        auto tile = localGrid[neighbor.row * cols + neighbor.col].get();
+        if (FoodTile* foodTile = dynamic_cast<FoodTile*>(tile)) {
+          food += foodTile->getWeight();
+          // Update the tile to an empty tile after the food is consumed
+          localGrid[neighbor.row * cols + neighbor.col] = std::make_unique<EmptyTile>();
+        }
+
+        if (food <= 0) {
+          continue;
+        }
 
         if (gScore.count(neighbor) == 0 || tentativeGScore < gScore[neighbor]) {
           cameFrom[neighbor] = current;
           gScore[neighbor] = tentativeGScore;
-          int fScore = tentativeGScore + manhattanDistance(neighbor, endPos);
+          foodMap[neighbor] = food;
+          int fScore = tentativeGScore + manhattanDistance(neighbor, endPos) + food;
           openSet.push({fScore, neighbor});
         }
       }
